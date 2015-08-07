cbdb94e3c90ce0c94d3b4a28155f54f6c5796269
Jason Grant <jason.grant@ualberta.ca>
2015-08-07 12:10:48 -0600

    change name of uploaded files

--- app/models/spectrum.rb
+++ app/models/spectrum.rb
@@ -3,7 +3,7 @@ class Spectrum < ActiveRecord::Base
   belongs_to :submission
 
   has_attached_file :spectrum_data,
-                    path: ':paperclip_dir/:filename'
+                    path: ':paperclip_dir/:paperclip_name.:extension'
 
   validates_attachment_file_name :spectrum_data, :matches => [/mzXML\Z/]
 
@@ -11,7 +11,21 @@ class Spectrum < ActiveRecord::Base
     Rails.root.join(self.submission.input_dir)
   end
 
+  def paperclip_name
+    case self.category
+    when 'blank'
+      'Blank'
+    when 'standards'
+      'Alkstd'
+    else
+      "Sample_#{self.id}"
+    end
+  end
+
   Paperclip.interpolates :paperclip_dir do |attachment, style|
     attachment.instance.paperclip_dir
+  end
+  Paperclip.interpolates :paperclip_name do |attachment, style|
+    attachment.instance.paperclip_name
   end
 end
