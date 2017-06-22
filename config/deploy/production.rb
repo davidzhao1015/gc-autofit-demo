e6a1f61f0817c206699fc3addfeae10d9eaf7e59
Arnau Serra-Cayuela <serracay@ualberta.ca>
2017-06-22 10:17:26 -0600

    Changed IP to deploy in the new server which is a snapshot from the previous one

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,5 +15,7 @@ set :stage, :production
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '104.154.69.124', user: 'gcms', roles: %w{web app db}
+#server '104.154.69.124', user: 'gcms', roles: %w{web app db}
+## new instance created from previous a snapshot
+server '104.198.157.103', user: 'gcms', roles: %w{web app db}
 
